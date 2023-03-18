2d252364dd01cab91b8111bd7696dbc9c6310754
DMemmott <daniellememmott@hotmail.com>
2023-03-18 15:21:42 -0600

    "move js and css into folder, ?s to js"

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+var jsQuiz = [
+    {
+        quest: 1,
+        answers: ["A", "B", "C", "D"],
+        right: ["B"],
+    },
+    {
+        quest: 2,
+        answers: ["A", "B", "C", "D"],
+        right: ["B"],
+    },
+    {
+        quest: 3,
+        answers: ["A", "B", "C", "D"],
+        right: ["B"],
+    },
+    {
+        quest: 4,
+        answers: ["A", "B", "C", "D"],
+        right: ["B"],
+    },
+    {   quest: 5,
+        answers: ["A", "B", "C", "D"],
+        right: ["B"],
+    }
+    ]
