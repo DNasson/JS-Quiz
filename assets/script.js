41be8ce3e2556d5ae6ef11339b2b33e1ddac8560
DMemmott <daniellememmott@hotmail.com>
2023-03-18 15:51:40 -0600

    "show ?s with options"

--- assets/script.js
+++ assets/script.js
@@ -22,5 +22,12 @@ var jsQuiz = [
     {   quest: 5,
         answers: ["A", "B", "C", "D"],
         right: ["B"],
+    },]
+
+    for (var i = 0; i < jsQuiz.length; i++) {
+            document.write(jsQuiz[i].quest + "<br />");
+      
+            for (var j = 0; j < jsQuiz[i].answers.length; j++) {
+              document.write("<input type=radio id=myRadio name=radAnswer>" + jsQuiz[i].answers[j] + "<br />");
     }
-    ]
+}
