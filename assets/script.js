ef114b7cb9215e9cc0feeb41ed7634343b087684
DMemmott <daniellememmott@hotmail.com>
2023-03-23 21:34:43 -0600

    "add js to show code"

--- assets/script.js
+++ assets/script.js
@@ -1,105 +1,122 @@
+var startButton = document.querySelector(".start");
+var timeEl = document.querySelector(".time");
+var mainEl = document.getElementById("main");
+var questionContainer = document.getElementById('question-container');
+var answersContainer = document.getElementById('answer-container');
+var answerButtons = document.querySelectorAll('button[class="answers"]');
+//console.log('answerButtons is', answerButtons);
+var userScore = 0;
+var questionIndex = 0;
+// Quiz questions and answers
 var jsQuiz = [
     {
         question: "What can be done on a webpage with JavaScript?",
-        answers: {
-            a: 'Click a button',
-            b: 'Retrieve data',
-            c: 'Fill out a form',
-            d: 'All of the above',
-        },
-
-        correctAnswer: 'd'
+        answers: [
+            'Click a button',
+            'Retrieve data',
+            'Fill out a form',
+            'All of the above',
+        ],
+        correctAnswer: 'All of the above'
     },
-
     {
         question: "What is a list of items surrounded by quotation marks called?",
-        answers: {
-            a: 'a list',
-            b: 'a lyric',
-            c: 'a string',
-            d: 'an array',
-        },
+        answers: [
+            'a list',
+            'a lyric',
+            'a string',
+            'an array',
+    ],
 
-        correctAnswer: 'c',
+        correctAnswer: 'a string',
     },
-
     {
         question: "How can the number 5 be added to a variable?",
-        answers: {
-            a: '5',
-            b: '"Five"',
-            c: 'Not possible, only letters are variables',
-            d: 'a and b',
-        },
+        answers: [
+            '5',
+            '"Five"',
+            'Not possible, only letters are variables',
+            'a and b',
+        ],
 
-        correctAnswer: 'd',
+        correctAnswer: 'a and b',
     },
-
     {
         question: "How do you call a function?",
-        answers: {
-            a: 'console.log',
-            b: 'Hey, function!',
-            c: 'Open Sesame!',
-            d: 'It is automatically called when defined',
-        },
+        answers: [
+            'console.log',
+            'Hey, function!',
+            'Open Sesame!',
+            'It is automatically called when defined',
+        ],
 
-        correctAnswer: 'a',
+        correctAnswer: 'console.log',
     },
-
     {
         question: "What do you open in DevTools to see output for JavaScript?",
-        answers: {
-            a: 'Styles',
-            b: 'Console',
-            c: 'Performance',
-            d: 'Recorder',
-        },
+        answers: [
+            'Styles',
+            'Console',
+            'Performance',
+            'Recorder',
+        ],
 
-        correctAnswer: 'b',
+        correctAnswer: 'Console',
     },
 ]
 
-myFunctionA=function(){
+myFunctionA = function () {
     console.log('a')
-} 
-myFunctionB=function(){
+}
+myFunctionB = function () {
     console.log('b')
-} 
-myFunctionC=function(){
+}
+myFunctionC = function () {
     console.log('c')
-} 
-myFunctionD=function(){
+}
+myFunctionD = function () {
     console.log('d')
-} 
+}
+// save button push in local storage
 var count = localStorage.getItem("letters");
-
 var aButton = document.querySelector("#a");
-aButton.addEventListener("click", function() {
+aButton.addEventListener("click", function () {
     localStorage.setItem("letters", "a")
 }
 )
-var score =0;
-
-var timeEl = document.querySelector(".time");
-
-var mainEl = document.getElementById("main");
-
+// seconds in count down
 var secondsLeft = 30;
+// shows question from hidden state and pulls questions and answers to main screen
+function showQuestion () {
+    questionContainer.classList.remove('hide');
+    answersContainer.classList.remove('hide');
+    questionContainer.textContent = jsQuiz[questionIndex]['question'];
+    for(let i = 0; i < 4; i++ ){
+        answerButtons[i].textContent = jsQuiz[questionIndex]['answers'][i];
 
-function startTime() {
-
-    var startButton = document.querySelector(".start");
-    startButton.addEventListener("click", function() {
-        event.preventDefault();
-        var timerInterval = setInterval (function() {
-            secondsLeft--;
-            timeEl.textContent = secondsLeft + " seconds left in quiz.";
-            if(secondsLeft === 0) {
-                clearInterval(timerInterval);
-            }  
-                 
-    }, 1000)
-}) 
+    }
 }
-startTime();
+// checks if answer is correct and adds 5 points for correct answers
+function checkUserAnswer(e) {
+    const thingClickedOn = e.target;
+    if(thingClickedOn.matches('button')) {
+        thingClickedOn.textContent == jsQuiz[questionIndex]['correctAnswer'] ? userScore = userScore + 5 : null;
+        console.log('userScore is now', userScore);
+    }
+    // increment my index now
+    questionIndex++;
+    showQuestion();
+}
+// event listeners
+answersContainer.addEventListener("click", checkUserAnswer);
+startButton.addEventListener("click", showQuestion);
+startButton.addEventListener("click", function () {
+    event.preventDefault();
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft + " seconds left in quiz.";
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+        }
+    }, 1000);
+}) 
